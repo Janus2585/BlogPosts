1a87436540d541c339644f0fe5664649c464d3e8
Janus2585 <bd.yhome@gmail.com>
2018-08-24 23:05:31 -0500

    Built renderPosts helper function in PostsIndex component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
 	//this is a lifecyle method. It will be automatically called by React immediately after the component is visible in the DOM
@@ -9,15 +10,35 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	//helper function to render the posts received from the api
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return(
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			);
+		});
+	}
+
 	render() {
+		console.log(this.props.posts);
 		return (
 			<div>
-				Posts Index
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
 }
 
+//whenever application level state is used, always use mapStateToProps
+function mapStateToProps (state) {
+	return { posts: state.posts };
+}
+
 //This is a shortcut instead of using mapStateToProps. The mapStateToProps argument is null because it is not used.
 //Instead of passing in mapStateToProps, the action creator fetchPosts is passed inside of an object (which is why the {} are there). 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
